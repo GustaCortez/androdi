5d6bc17b55d80ba46fb923372f5b733b702f1e74
GustaCortez <melvincortey@gmail.com>
2022-08-19 21:04:43 -0600

    unoL

---
+++ HeelloWord/app/src/main/java/com/androidavanzado/heelloword/MainActivity.kt
@@ -0,0 +1,1 @@
+package com.androidavanzado.heellowordimport androidx.appcompat.app.AppCompatActivityimport android.os.Bundleclass MainActivity : AppCompatActivity() {    override fun onCreate(savedInstanceState: Bundle?) {        super.onCreate(savedInstanceState)        setContentView(R.layout.activity_main)       // EJEMPLO DE FICHERO    }}
